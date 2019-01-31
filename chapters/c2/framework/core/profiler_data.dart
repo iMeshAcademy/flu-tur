90b65d6be99a54ae3bd2cbf02fb92b052de1dac1
ajeeshklr <ajeeshklr@gmail.com>
2019-01-31 17:09:27 +0530

    License, Author, and copy right updated

--- chapters/c2/framework/core/profiler_data.dart
+++ chapters/c2/framework/core/profiler_data.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2019, iMeshAcademy authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 class ProfilerData {
   final String category;
   final String description;
